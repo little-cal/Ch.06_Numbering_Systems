b07a550072b253dc857327a1b2f1ef1a9c4a848e
little-cal <littlecal@urbandaleschools.com>
2023-11-15 19:15:05 -0600

    11/15/2023

--- ASCII tests.py
+++ ASCII tests.py
@@ -1,8 +1,9 @@
-secret = (64, 59, 58, 63)
-
-
-for j in range(2, 21):
-    for i in secret:
-        i += j
-        print(chr(i), end="")
-    print()
+# secret = (64, 59, 58, 63)
+#
+#
+# for j in range(2, 21):
+#     for i in secret:
+#         i += j
+#         print(chr(i), end="")
+#     print()
+print(chr(90))
